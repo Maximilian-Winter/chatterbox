8ac86c4132bf7be9e99e5ce15309236501fda04c
Maximilian Winter <maximilian.winter.91@gmail.com>
2025-09-19 01:46:19 +0200

    a

--- example_tts.py
+++ example_tts.py
@@ -26,6 +26,6 @@ ta.save("test-2.wav", wav, multilingual_model.sr)
 
 
 # If you want to synthesize with a different voice, specify the audio prompt
-AUDIO_PROMPT_PATH = "YOUR_FILE.wav"
+AUDIO_PROMPT_PATH = "voice.wav"
 wav = model.generate(text, audio_prompt_path=AUDIO_PROMPT_PATH)
 ta.save("test-3.wav", wav, model.sr)
